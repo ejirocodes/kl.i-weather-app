ff74b2f722dafa9c6282419a48620389b72b48bf
Ejiro Asiuwhu <Ejiroasiuwhu10@gmail.com>
2022-06-17 00:26:16 +0100

    style: card info style, add weather icons

--- src/components/WeatherInfo.vue
+++ src/components/WeatherInfo.vue
@@ -3,16 +3,53 @@
 </script>
 <template>
   <section class="tw-bg-pri">
-    <h1>Nigeira</h1>
-    <p>Chances of Rain: 5%</p>
+    <h1 class="tw-text-4xl tw-font-semibold">Nigeira</h1>
+    <p class="tw-text-xl tw-font-medium">Chances of Rain: 5%</p>
 
-    <div>
-      <h2>28&deg;</h2>
+    <div class="tw-mb-28">
+      <h class="tw-text-9xl tw-font-bold tw-relative temp">
+        28<span class="tw-absolute tw-top-0 tw-left-[138px] tw-text-8xl deg"
+          >&deg;</span
+        ></h
+      >
       <img src="@/assets/img/rainyday-light.svg" alt="thunder" />
     </div>
+    <div>
+      <img src="" alt="" />
+    </div>
+    <div>
+      <div>
+        <img src="@/assets/img/sunrise-light.svg" alt="Sunrise" />
+        Sunrise
+      </div>
+      <div>
+        <img src="@/assets/img/humidity-light.svg" alt="Humidity" />
+        Humidity
+      </div>
+      <div>
+        <img src="@/assets/img/wind-light.svg" alt="Wind" />
+        Wind
+      </div>
 
+      <div>
+        <img src="@/assets/img/mostly-cloudy-light.svg" alt="Pressure" />
+        Pressure
+      </div>
+    </div>
     <!-- <div>{{ generateTime() }}</div> -->
+    <!-- {{ new Date().toLocaleDateString() }} -->
   </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+.temp {
+  background: -webkit-linear-gradient(-90deg, #eeeeee 40%, transparent);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.temp .deg {
+  background: -webkit-linear-gradient(221deg, #eeeeee, transparent 60%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
