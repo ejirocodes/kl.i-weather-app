8689956fbd5bbe80d8c2b33dd97b9f95bcdb6c33
Ejiro Asiuwhu <Ejiroasiuwhu10@gmail.com>
2022-06-17 00:51:11 +0100

    style(ui): style weather info component

--- src/components/WeatherInfo.vue
+++ src/components/WeatherInfo.vue
@@ -2,37 +2,57 @@
 // import { generateTime } from "@/utils/date.utils";
 </script>
 <template>
-  <section class="tw-bg-pri">
-    <h1 class="tw-text-4xl tw-font-semibold">Nigeira</h1>
-    <p class="tw-text-xl tw-font-medium">Chances of Rain: 5%</p>
+  <section
+    class="tw-flex tw-w-[40rem] tw-flex-col tw-items-center tw-rounded-[2.4rem] tw-bg-pri tw-py-16 tw-text-center tw-text-white"
+  >
+    <h1 class="tw-mb-2 tw-text-4xl tw-font-semibold">Nigeira</h1>
+    <p class="tw-mb-14 tw-text-xl tw-font-medium tw-text-slate-300">
+      Chances of Rain: 5%
+    </p>
 
-    <div class="tw-mb-28">
-      <h class="tw-text-9xl tw-font-bold tw-relative temp">
-        28<span class="tw-absolute tw-top-0 tw-left-[138px] tw-text-8xl deg"
+    <div class="tw-mb-14">
+      <h class="temp tw-relative tw-text-[10rem] tw-font-bold">
+        28<span class="deg tw-absolute -tw-top-3 tw-left-[178px] tw-text-[6rem]"
           >&deg;</span
         ></h
       >
-      <img src="@/assets/img/rainyday-light.svg" alt="thunder" />
+      <img
+        src="@/assets/img/rainyday-light.svg"
+        alt="thunder"
+        class="-tw-mt-16"
+      />
     </div>
     <div>
       <img src="" alt="" />
     </div>
-    <div>
-      <div>
-        <img src="@/assets/img/sunrise-light.svg" alt="Sunrise" />
+    <div class="tw-flex tw-items-center tw-justify-center">
+      <div class="tw-mr-8">
+        <img
+          src="@/assets/img/sunrise-light.svg"
+          alt="Sunrise"
+          class="tw-w-20"
+        />
         Sunrise
       </div>
-      <div>
-        <img src="@/assets/img/humidity-light.svg" alt="Humidity" />
+      <div class="tw-mr-8">
+        <img
+          src="@/assets/img/humidity-light.svg"
+          alt="Humidity"
+          class="tw-w-20"
+        />
         Humidity
       </div>
-      <div>
-        <img src="@/assets/img/wind-light.svg" alt="Wind" />
+      <div class="tw-mr-8">
+        <img src="@/assets/img/wind-light.svg" alt="Wind" class="tw-w-20" />
         Wind
       </div>
 
       <div>
-        <img src="@/assets/img/mostly-cloudy-light.svg" alt="Pressure" />
+        <img
+          src="@/assets/img/mostly-cloudy-light.svg"
+          alt="Pressure"
+          class="tw-w-20"
+        />
         Pressure
       </div>
     </div>
@@ -42,14 +62,14 @@
 </template>
 
 <style scoped>
-.temp {
-  background: -webkit-linear-gradient(-90deg, #eeeeee 40%, transparent);
-  -webkit-background-clip: text;
+.temp,
+.temp .deg {
+  background: -webkit-linear-gradient(-90deg, #eeeeee 20%, transparent 80%);
+  -webkit-background-clip: text !important;
+  background-clip: text !important;
   -webkit-text-fill-color: transparent;
 }
 .temp .deg {
   background: -webkit-linear-gradient(221deg, #eeeeee, transparent 60%);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
 }
 </style>
